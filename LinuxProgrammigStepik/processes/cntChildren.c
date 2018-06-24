bbcdcf9f40121cf08a54b613498b603deda180f8
Dima Timofeev <dimkat@gmail.com>
2018-06-24 19:03:27 +0000

    Count how many children the process has

--- LinuxProgrammigStepik/processes/cntChildren.c
+++ LinuxProgrammigStepik/processes/cntChildren.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <string.h>
 
-#define MAX_PIDS 32767
+#define MAX_PIDS 1000 
 
-int getProcessWithParent() {
-	int pids[MAX_PIDS];
-	for (int i=0; i<MAX_PIDS; i++)
-		pids[i] = -1;
+static int cnt = 1;
 
-	DIR* proc = opendir("/proc");
-	struct dirent* de;
-
-	int cnt = 0;
-
-	while ((de = readdir(proc)) != NULL) {
-		if (strstr(de->d_name, ".") != NULL)
-			continue;
-		if (strstr(de->d_name, ".") != NULL)
-			continue;
-		if (de->d_type == DT_DIR && atoi(de->d_name) > 0) {
-			pids[cnt] = atoi(de->d_name);
+void findChildren(int pid, const int *pids, const int *ppids) {
+	int i;
+	for (i=0; i<MAX_PIDS; i++) {
+		if (pid == ppids[i]) {
 			cnt++;
+			findChildren(pids[i], pids, ppids);
 		}
 	}
-	
-	closedir(proc);
-	return cnt;
 }
 
-int main() {
-	int c = getProcessWithParent();
-	printf("Processes: %i\n", c);
+int main(int c, char **v) {
+	int pids[MAX_PIDS];
+	int ppids[MAX_PIDS];
+	
+	FILE *f = popen("cat /proc/*/status | grep '\\(^Pid\\|^PPid\\)'", "r"); 
+	int i = 0;
+	char pidstr[256]; char ppidstr[256];
+	while (fgets(pidstr, sizeof(pidstr), f)) {
+		fgets(ppidstr, sizeof(ppidstr), f);
+		
+		// Copy process IDs
+		char pid[21];
+		pid[20] = '\0';
+		strncpy(pid, pidstr+5, 20);
+		pids[i] = atoi(pid);
+		// Copy parent process IDs
+		char ppid[21];
+		ppid[20] = '\0';
+		strncpy(ppid, ppidstr+5, 20);
+		ppids[i] = atoi(ppid);
+
+		i++;
+	}
+	pclose(f);
+	
+	// Main logic
+	findChildren(atoi(v[1]), pids, ppids);
+	printf("%i\n", cnt);
 	return 0;
 }
