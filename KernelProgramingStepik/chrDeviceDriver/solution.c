417f0dcf09fff1a02bce440c63ce8124874b4d2c
Dima Timofeev <dimkat@gmail.com>
2018-09-12 08:12:50 +0100

    Char device with dynamic major number allocation

--- KernelProgramingStepik/chrDeviceDriver/solution.c
+++ KernelProgramingStepik/chrDeviceDriver/solution.c
@@ -1,7 +1,9 @@
 #include <linux/cdev.h>
+#include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
@@ -9,20 +11,20 @@
 #define KBUF_SIZE 10 * PAGE_SIZE
 #define MYDEV_NAME "solution_node"
 
+static char *node_name = "default_node_name";
+module_param(node_name, charp, 0660);
+
 // Module-level varaibles
-static dev_t first;
-static int my_major = 240, my_minor = 0;
-static struct cdev *my_cdev;
-static unsigned int devcnt = 1;
-static int opencnt = 0; // How many times the device was opened for read or write.
-static int datacnt = 0; // An amount of data written into the device.
 static char message[100] = {0};
+static int given_major;  
+static int my_minor = 0;
+static struct device *my_cdev;
+static struct class *my_class;
 
 
 static int mychrdev_open(struct inode* inode, struct file* file) {
 	char *kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
 	file->private_data = kbuf;
-	opencnt++;
   return 0;
   }
 
@@ -36,16 +38,8 @@ static int mychrdev_release(struct inode* inode, struct file* file) {
 	return 0;
   }
 
-/** @brief This function is called whenever device is being read from user space i.e. data is
- *  being sent from the device to the user. In this case is uses the copy_to_user() function to
- *  send the buffer string to the user and captures any errors.
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- *  @param buffer The pointer to the buffer to which this function writes the data
- *  @param len The length of the b
- *  @param offset The offset if required
- */
 static ssize_t mychrdev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
-	snprintf(message, sizeof(message), "%d %d\n", opencnt, datacnt);
+	snprintf(message, sizeof(message), "%d\n", given_major);
 	return simple_read_from_buffer(buffer, len, offset, message, sizeof(message));
 }
 
@@ -53,8 +47,6 @@ static ssize_t mychrdev_write(struct file *file, const  char __user *buf, size_t
   char *kbuf = file->private_data;
 	int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
 	ppos += nbytes;
-  datacnt += nbytes;
-	printk(KERN_INFO "Data count: %d\n", datacnt);
 	return nbytes;
 }
 
@@ -67,26 +59,42 @@ static const struct file_operations mycdev_fops = {
 };
 
 static int __init init_chardev(void) {
-	int err;
-	first = MKDEV(my_major, my_minor);
-	register_chrdev_region(first, devcnt, MYDEV_NAME);
-	my_cdev = cdev_alloc();	
-	cdev_init(my_cdev, &mycdev_fops);
-	err = cdev_add(my_cdev, first, devcnt);
-	if (err < 0) {
-		printk(KERN_ERR "Device add error!\n");
-		return err;
-	}
-	printk(KERN_INFO "Module 'solution_node' loaded succesfully.\n");
-	return 0;
+
+  // Try to dynamically allocate a major number for the device -- more difficult but worth it
+  given_major = register_chrdev(0, node_name, &mycdev_fops);
+  if (given_major < 0){
+    printk(KERN_ALERT "MyCharDev: failed to register a major number\n");
+    return given_major;
+  }
+  printk(KERN_INFO "MyCharDev: registered correctly with major number %d\n", given_major);
+
+  // Register the device class
+  my_class = class_create(THIS_MODULE, "my_class");
+  if (IS_ERR(my_class)) {                // Check for error and clean up if there is
+      unregister_chrdev(given_major, node_name);
+      printk(KERN_ALERT "Failed to register device class\n");
+      return PTR_ERR(my_class);          // Correct way to return an error on a pointer
+  }
+  printk(KERN_INFO "MyCharDev: device class registered correctly\n");
+
+  // Register the device driver
+  my_cdev = device_create(my_class, NULL, MKDEV(given_major, my_minor), NULL, node_name);
+  if (IS_ERR(my_cdev)) {               // Clean up if there is an error
+      class_destroy(my_class);           // Repeated code but the alternative is goto statements
+      unregister_chrdev(given_major, node_name);
+      printk(KERN_ALERT "Failed to create the device\n");
+      return PTR_ERR(my_cdev);
+  }
+  printk(KERN_INFO "MyCharDev: device class created correctly\n"); // Made it! device was initialized
+  return 0;
 }
 
 static void __exit cleanup_chardev(void) {
-	if (my_cdev) {
-		cdev_del(my_cdev);
-	}
-	unregister_chrdev_region(first, devcnt);
-	printk(KERN_INFO "Module 'solution_node' cleaned up succesfully.\n");
+  device_destroy(my_class, MKDEV(given_major, 0));     // remove the device
+  class_unregister(my_class);                          // unregister the device class
+  class_destroy(my_class);                             // remove the device class
+  unregister_chrdev(given_major, node_name);             // unregister the major number
+	printk(KERN_INFO "MyCharDev: Module 'solution_node' cleaned up succesfully.\n");
 }
 
 module_init(init_chardev);
